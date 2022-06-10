849ae1fe96c4ba7371279a110f2876a94455f101
Haibin Zhao <zhb950102@qq.com>
2022-06-10 20:11:26 +0200

    figures for visualization

---
+++ notebooks/wavelet filters/FigureConfig.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+# plt.rcParams["font.family"] = "Times New Roman"
+
+Green    = np.array([0, 150, 130]) / 255
+Pink     = np.array([255, 105, 125]) / 255
+Blue     = np.array([70, 100, 170]) / 255
+Braun    = np.array([185, 155, 85]) / 255
+Black    = np.array([64, 64, 64]) / 255
+Gray     = np.array([191, 191, 191]) / 255
+Cyan     = np.array([35, 161, 224]) / 255
+Orange   = np.array([220, 94, 31]) / 255
+Lila     = np.array([163, 16, 124]) / 255
+Maigreen = np.array([140, 182, 60]) / 255
+Yellow   = np.array([252, 229, 0]) / 255
+Red      = np.array([162, 34, 35]) / 255
+        
+liste    = np.array([Green, Pink,     Blue,   Braun,
+                     Black, Gray,     Cyan,   Orange,
+                     Lila,  Maigreen, Yellow, Red])
+
+
+LW       = 10
