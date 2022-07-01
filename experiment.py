39d5bff379b3260970b75716cf581c33d842a925
Yexu Zhou <zhou@teco.edu>
2022-07-01 18:53:12 +0200

    setting name

--- experiment.py
+++ experiment.py
@@ -103,26 +103,28 @@ class Exp(object):
         if self.args.model_type == "deepconvlstm":
             config_file = open('../../configs/model.yaml', mode='r')
             config = yaml.load(config_file, Loader=yaml.FullLoader)["deepconvlstm"]
-            setting = "deepconvlstm_data_{}_seed_{}_windowsize_{}_waveFilter_{}_Fscaling_{}_cvfilter_{}_lstmfilter_{}_Regu_{}".format(self.args.data_name,
-                                                                                                                                      self.args.seed,
-                                                                                                                                      self.args.windowsize,
-                                                                                                                                      self.args.wavelet_filtering,
-                                                                                                                                      self.args.filter_scaling_factor,
-                                                                                                                                      config["nb_filters"],
-                                                                                                                                      config["nb_units_lstm"],
-                                                                                                                                      self.args.wavelet_filtering_regularization)
+            setting = "deepconvlstm_data_{}_seed_{}_windowsize_{}_waveFilter_{}_Fscaling_{}_cvfilter_{}_lstmfilter_{}_Regu_{}_wavelearnble_{}".format(self.args.data_name,
+                                                                                                                                                        self.args.seed,
+                                                                                                                                                        self.args.windowsize,
+                                                                                                                                                        self.args.wavelet_filtering,
+                                                                                                                                                        self.args.filter_scaling_factor,
+                                                                                                                                                        config["nb_filters"],
+                                                                                                                                                        config["nb_units_lstm"],
+                                                                                                                                                        self.args.wavelet_filtering_regularization,
+                                                                                                                                                        self.args.wavelet_filtering_learnable )
             return setting
         elif self.args.model_type == "attend":
             config_file = open('../../configs/model.yaml', mode='r')
             config = yaml.load(config_file, Loader=yaml.FullLoader)["attend"]
-            setting = "attend_data_{}_seed_{}_windowsize_{}_waveFilter_{}_Fscaling_{}_cvfilter_{}_grufilter_{}_Regu_{}".format(self.args.data_name,
-                                                                                                                               self.args.seed,
-                                                                                                                               self.args.windowsize,
-                                                                                                                               self.args.wavelet_filtering,
-                                                                                                                               self.args.filter_scaling_factor,
-                                                                                                                               config["filter_num"],
-                                                                                                                               config["hidden_dim"],
-                                                                                                                               self.args.wavelet_filtering_regularization)
+            setting = "attend_data_{}_seed_{}_windowsize_{}_waveFilter_{}_Fscaling_{}_cvfilter_{}_grufilter_{}_Regu_{}_wavelearnble_{}".format(self.args.data_name,
+                                                                                                                                               self.args.seed,
+                                                                                                                                               self.args.windowsize,
+                                                                                                                                               self.args.wavelet_filtering,
+                                                                                                                                               self.args.filter_scaling_factor,
+                                                                                                                                               config["filter_num"],
+                                                                                                                                               config["hidden_dim"],
+                                                                                                                                               self.args.wavelet_filtering_regularization
+                                                                                                                                               self.args.wavelet_filtering_learnable)
             return setting
         else:
             raise NotImplementedError
