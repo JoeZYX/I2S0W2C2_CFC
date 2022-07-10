7a09248d48a88fe5458e5a23d27fabe9e266e124
Yexu Zhou <zhou@teco.edu>
2022-07-10 18:28:33 +0200

    uschad

--- dataloaders/__init__.py
+++ dataloaders/__init__.py
@@ -62,6 +62,7 @@ from .dataloader_PAMAP_har import PAMAP2_HAR_DATA
 from .dataloader_SKODAR_har import SkodaR_HAR_DATA
 from .dataloader_DSADS_har import DSADS_HAR_DATA
 from .dataloader_DG_har import Daphnet_HAR_DATA
+from .dataloader_USCHAD_har import USC_HAD_HAR_DATA
 data_dict = {"hapt"  : HAPT_HAR_DATA,
              "ear"   : EAR_HAR_DATA,
              "oppo"  : Opportunity_HAR_DATA,
@@ -69,7 +70,8 @@ data_dict = {"hapt"  : HAPT_HAR_DATA,
              "pamap2": PAMAP2_HAR_DATA,
              "skodar": SkodaR_HAR_DATA,
              "dsads" : DSADS_HAR_DATA,
-             "dg"    : Daphnet_HAR_DATA}
+             "dg"    : Daphnet_HAR_DATA,
+             "uschad": USC_HAD_HAR_DATA}
 
 class data_set(Dataset):
     def __init__(self, args, dataset, flag):
